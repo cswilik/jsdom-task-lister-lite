654e8e45353d838f4022dc31d37e13c736953684
CSWILIK <cswilik@gmail.com>
2020-12-16 16:08:45 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let createTask = document.querySelector("#create-task-form")
+let taskUL = document.querySelector("#tasks")
+
+createTask.addEventListener("submit", function(evt) {
+  evt.preventDefault()
+  // prevents page from reloading
+  taskDescription = document.querySelector("#new-task-description") 
+  // grabbing the tag for the input from user
+  liTask = document.createElement("li")
+  // creating li tag
+  liTask.innerText = taskDescription.value
+  liTask.addEventListener("click", function(evt){
+    evt.target.remove()
+  })
+  
+  // changing text of li take to input value from user
+  taskUL.append(liTask)
+
+  // attaching it to the DOM
+  evt.target.reset()
+
+  
+
+})
+
+
+
+
+
 });
